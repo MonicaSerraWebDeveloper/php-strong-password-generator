95088984ffb2cc801bc5358c5d6ed2bb25e343fd
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-05-03 15:44:08 +0200

    feat: add a functions.php new file and with "require" is calledback in index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<?php 
-
-    $numberLengthPassword = isset($_GET['length-password']) ? intval($_GET['length-password']) : '';
-    $officialPassword = generatePassword($numberLengthPassword);
-
-    function generatePassword($number) {
-        $alphaNumericString = 'abcdefghijklmnopqrstuvzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@.<>-_';
-        $newPassword = '';
-        if (!empty($_GET['length-password'])) {
-            for($i = 0; $i <= $number; $i++) {
-                $randomIndex = rand(0, strlen($alphaNumericString));  
-                $newPassword .= $alphaNumericString[$randomIndex];   
-            };
-            return $newPassword;
-        }
-        
-    };
-
-
-?>
+<?php require __DIR__ . '/functions.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
